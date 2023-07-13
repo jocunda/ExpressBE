1ad7057e95837b16b505c4127ea4e849cc077e18
jocunda <silviawanshan88@gmail.com>
2023-07-13 21:26:22 +0800

    Feat:change route for itemid

--- src/routes/items.js
+++ src/routes/items.js
@@ -35,4 +35,22 @@ router.get("/itemlist", async (request, response) => {
   }
 });
 
+router.get("/:itemId", async (request, response) => {
+  const token = request.session.token;
+  const { itemId } = request.params;
+
+  if (!token) return response.sendStatus(401);
+
+  try {
+    const [item] = await db
+      .promise()
+      .query(`SELECT * FROM ITEMS WHERE id = ?`, [itemId]);
+
+    response.status(200).json(item[0]);
+  } catch (err) {
+    console.log(err);
+    response.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
 module.exports = router;
