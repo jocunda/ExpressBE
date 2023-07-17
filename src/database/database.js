44635faec9aae0d5eb6e7c1721c537f2f8b8bc07
jocunda <silviawanshan88@gmail.com>
2023-07-17 17:27:04 +0800

    Feat:change db setting

--- src/database/database.js
+++ src/database/database.js
@@ -1,8 +1,8 @@
 const mysql = require("mysql2");
 
 module.exports = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
 });
