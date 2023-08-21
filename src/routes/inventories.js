1361c1e798463905041ffd506800106acd9a2f1c
jocunda <silviawanshan88@gmail.com>
2023-08-21 22:28:58 +0800

    Feat:add inventories router

--- src/routes/inventories.js
+++ src/routes/inventories.js
@@ -49,22 +49,6 @@ router.get("/inventorieslist/:itemId", async (request, response) => {
   }
 });
 
-// router.get("/:itemId", async (request, response) => {
-//   const token = request.session.token;
-//   const { itemId } = request.params;
-
-//   if (!token) return response.sendStatus(401);
-
-//   try {
-//     const [item] = await db.query(`SELECT * FROM items WHERE id = ?`, [itemId]);
-
-//     response.status(200).json(item[0]);
-//   } catch (err) {
-//     console.log(err);
-//     response.status(500).json({ message: "Internal Server Error" });
-//   }
-// });
-
 router.post("/addInventory", async (request, response) => {
   const token = request.session.token;
   if (!token) return response.sendStatus(401);
@@ -91,9 +75,10 @@ router.post("/addInventory", async (request, response) => {
     do {
       inventoryId = uuidv4();
       try {
+        const timestamp = new Date();
         await db.query(
-          `INSERT INTO inventories (id, no, qty, memo, itemId) VALUES (?, ?, ?, ?, ?)`,
-          [inventoryId, code, quantity, memo, itemId]
+          `INSERT INTO inventories (id, no, qty, memo, itemId, createdDate) VALUES (?, ?, ?, ?, ?, ?)`,
+          [inventoryId, code, quantity, memo, itemId, timestamp]
         );
 
         //to update deleteable inside item
@@ -127,6 +112,102 @@ router.post("/addInventory", async (request, response) => {
   } catch (err) {
     console.log(err);
     response.status(500).json({ message: "Failed to add inventory" });
+  }
+});
+
+router.delete("/delete/:inventoryId", async (request, response) => {
+  const token = request.session.token;
+  const { inventoryId } = request.params;
+
+  if (!token) return response.sendStatus(401);
+
+  try {
+    const [inventory] = await db.query(
+      `SELECT * FROM inventories WHERE id = ?`,
+      [inventoryId]
+    );
+
+    if (inventory.length === 0) {
+      // If the item doesn't exist, return a 404 Not Found status.
+      return response.status(404).json({ message: "Inventory not found" });
+    }
+
+    await db.query(`DELETE FROM inventories WHERE id = ?`, [inventoryId]);
+    //to update deleteable inside item
+    await db.query(`
+          UPDATE items as leftItems, 
+          (
+            SELECT a.id,
+              CASE
+                WHEN count(b.id)= 0 THEN true
+                ELSE false
+              END as deleteable 
+            FROM items as a 
+            LEFT JOIN inventories as b 
+            ON a.id = b.itemId
+            GROUP BY a.id
+          ) as rightItems 
+          SET leftItems.deleteable = rightItems.deleteable 
+          WHERE leftItems.id = rightItems.id;
+        `);
+    response.json({ message: "Inventory deleted successfully" });
+  } catch (err) {
+    console.log(err);
+    response.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+router.put("/edit/:inventoryId", async (request, response) => {
+  const token = request.session.token;
+  const { inventoryId } = request.params;
+
+  if (!token) return response.sendStatus(401);
+
+  const { value, code, description } = request.body;
+  if (!value || !code) {
+    return response
+      .status(400)
+      .json({ message: "Name and code of Inventory is required!" });
+  }
+
+  try {
+    const [currentItemRows] = await db.query(
+      `SELECT * FROM items WHERE id = ?`,
+      [itemId]
+    );
+
+    if (currentItemRows.length === 0) {
+      return response.status(404).json({ message: "Item not found!" });
+    }
+
+    // Check for existing items with the same value and code but not the same item.
+    const [existingRows] = await db.query(
+      `SELECT * FROM items WHERE (value = ? OR code = ?) AND id <> ?`,
+      [value, code, itemId]
+    );
+
+    if (existingRows.length > 0) {
+      const existingItem = existingRows[0];
+      if (existingItem.value === value) {
+        return response
+          .status(409)
+          .json({ message: "Item with the same value already exists!" });
+      }
+      if (existingItem.code === code) {
+        return response
+          .status(409)
+          .json({ message: "Item with the same code already exists!" });
+      }
+    }
+
+    await db.query(
+      `UPDATE items SET value=?, code=?, description=? WHERE id=?`,
+      [value, code, description, itemId]
+    );
+    response.json({ message: "Item updated successfully" });
+  } catch (err) {
+    console.log(err);
+    response.status(500).json({ message: "Failed to update item" });
   }
 });
 
